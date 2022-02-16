385e7b044b6903895ace3c5b2deedd441623c0c7
edumassud <58275179+edumassud@users.noreply.github.com>
2022-02-15 19:41:21 -0500

    Pretty much done
    Last circle doesnt react on very first click

---
+++ main.js
@@ -0,0 +1,23 @@
+function circle1Clicked() {
+    let count = document.getElementById("circle1Text").innerHTML;
+    count++;
+    document.getElementById("circle1Text").innerHTML = count;
+}
+
+function circle2MouseOver() {
+    document.getElementById("circle2").style.stroke = "black";
+    document.getElementById("circle2").style.strokeWidth = "10px";
+}
+
+function circle2MouseOut() {
+    // document.getElementById("circle2").style.stroke = "black";
+    document.getElementById("circle2").style.strokeWidth = "0px";
+}
+
+function circle3Clicked() {
+    if (document.getElementById("circle3").style.opacity == "1") {
+        document.getElementById("circle3").style.opacity = "0"
+    } else {
+        document.getElementById("circle3").style.opacity = "1"
+    }
+}
